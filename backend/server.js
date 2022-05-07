324d3da4262f8d181329a331a7e411c81b116332
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-07 03:58:44 -0600

    mern

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,22 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 1660;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/main", require("./routes/main"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, () => {
   console.log("port", port);
