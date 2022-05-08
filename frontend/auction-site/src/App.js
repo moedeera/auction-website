4274276cb622cb2e4ff26563bff1003c33837b3a
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-08 10:02:14 -0600

    main-pag0

--- frontend/auction-site/src/App.js
+++ frontend/auction-site/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Homepage } from "./Pages/Homepage/Homepage";
 import { Navbar } from "./Pages/Components/navbar/Navbar";
+import { Footer } from "./Pages/Components/footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
         </Routes>
       </Router>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/auction-site/src/Pages/Components/footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <nav className="footer-main">
+        <ul>
+          <h3>EXPLORE</h3>
+          <li>About</li>
+          <li>Join</li>
+          <li>Advertise</li>
+        </ul>
+        <ul>
+          <h3>INFO</h3>
+          <li>Terms</li>
+          <li>Privacy</li>
+          <li>Posting</li>
+          <li>Support</li>
+        </ul>
+        <ul>
+          <h3>SUPPORT</h3>
+          <li>Contact</li>
+          <li>ESP</li>
+          <li>Accessibility</li>
+        </ul>
+      </nav>
+      <ul className="footer-social-media">
+        <i className="fab fa-facebook fa-2x"></i>
+        <i className="fab fa-google-plus-square fa-2x"></i>
+        <i className="fab fa-twitter fa-2x"></i>
+        <i className="fab fa-instagram-square fa-2x"></i>
+      </ul>
+      <small>Copyright &copy; 2018, All Right Reserved</small>
+    </div>
+  );
+};
