a10190432a6f7213d9c988da3627fd3bffa45b12
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-09 08:06:57 -0600

    committ

---
+++ frontend/auction-site/src/Pages/Components/item/Item.js
@@ -0,0 +1,130 @@
+import React from "react";
+import "./Item.css";
+
+export const Item = () => {
+  return (
+    <div className="items">
+      {" "}
+      <div className="posted-item">
+        <div className="item-images">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
+            alt=""
+          />
+        </div>
+        <div className="item-actions">
+          <div className="item-info header">
+            <h3>Audi BMQ 2011</h3>
+            <p>15,560$</p>
+          </div>
+          <div className="item-timer">
+            <p>2 days and 15 hours left on the bid</p>
+          </div>
+          <div className="item-info">
+            <button>Place a bid</button>
+          </div>
+        </div>
+      </div>
+      <div className="posted-item">
+        <div className="item-images">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
+            alt=""
+          />
+        </div>
+        <div className="item-actions">
+          <div className="item-info header">
+            <h3>Audi BMQ 2011</h3>
+            <p>15,560$</p>
+          </div>
+          <div className="item-timer">
+            <p>2 days and 15 hours left on the bid</p>
+          </div>
+          <div className="item-info">
+            <button>Place a bid</button>
+          </div>
+        </div>
+      </div>
+      <div className="posted-item">
+        <div className="item-images">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
+            alt=""
+          />
+        </div>
+        <div className="item-actions">
+          <div className="item-info header">
+            <h3>Audi BMQ 2011</h3>
+            <p>15,560$</p>
+          </div>
+          <div className="item-timer">
+            <p>2 days and 15 hours left on the bid</p>
+          </div>
+          <div className="item-info">
+            <button>Place a bid</button>
+          </div>
+        </div>
+      </div>
+      <div className="posted-item">
+        <div className="item-images">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
+            alt=""
+          />
+        </div>
+        <div className="item-actions">
+          <div className="item-info header">
+            <h3>Audi BMQ 2011</h3>
+            <p>15,560$</p>
+          </div>
+          <div className="item-timer">
+            <p>2 days and 15 hours left on the bid</p>
+          </div>
+          <div className="item-info">
+            <button>Place a bid</button>
+          </div>
+        </div>
+      </div>
+      <div className="posted-item">
+        <div className="item-images">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
+            alt=""
+          />
+        </div>
+        <div className="item-actions">
+          <div className="item-info header">
+            <h3>Audi BMQ 2011</h3>
+            <p>15,560$</p>
+          </div>
+          <div className="item-timer">
+            <p>2 days and 15 hours left on the bid</p>
+          </div>
+          <div className="item-info">
+            <button>Place a bid</button>
+          </div>
+        </div>
+      </div>
+      <div className="posted-item">
+        <div className="item-images">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
+            alt=""
+          />
+        </div>
+        <div className="item-actions">
+          <div className="item-info header">
+            <h3>Audi BMQ 2011</h3>
+            <p>15,560$</p>
+          </div>
+          <div className="item-timer">
+            <p>2 days and 15 hours left on the bid</p>
+          </div>
+          <div className="item-info">
+            <button>Place a bid</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/auction-site/src/Pages/Homepage/Homepage.js
+++ frontend/auction-site/src/Pages/Homepage/Homepage.js
@@ -1,135 +1,13 @@
 import React from "react";
 import "./Homepage.css";
+import { Item } from "../Components/item/Item";
 
 export const Homepage = () => {
   return (
     <div className="main-container">
       {/* <div className="promotion">promotion</div> */}
-      <div className="recommendations">
-        <div className="items">
-          {" "}
-          <div className="posted-item">
-            <div className="item-images">
-              <img
-                src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
-                alt=""
-              />
-            </div>
-            <div className="item-actions">
-              <div className="item-info header">
-                <h3>Audi BMQ 2011</h3>
-                <p>15,560$</p>
-              </div>
-              <div className="item-timer">
-                <p>2 days and 15 hours left on the bid</p>
-              </div>
-              <div className="item-info">
-                <button>Place a bid</button>
-              </div>
-            </div>
-          </div>
-          <div className="posted-item">
-            <div className="item-images">
-              <img
-                src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
-                alt=""
-              />
-            </div>
-            <div className="item-actions">
-              <div className="item-info header">
-                <h3>Audi BMQ 2011</h3>
-                <p>15,560$</p>
-              </div>
-              <div className="item-timer">
-                <p>2 days and 15 hours left on the bid</p>
-              </div>
-              <div className="item-info">
-                <button>Place a bid</button>
-              </div>
-            </div>
-          </div>
-          <div className="posted-item">
-            <div className="item-images">
-              <img
-                src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
-                alt=""
-              />
-            </div>
-            <div className="item-actions">
-              <div className="item-info header">
-                <h3>Audi BMQ 2011</h3>
-                <p>15,560$</p>
-              </div>
-              <div className="item-timer">
-                <p>2 days and 15 hours left on the bid</p>
-              </div>
-              <div className="item-info">
-                <button>Place a bid</button>
-              </div>
-            </div>
-          </div>
-          <div className="posted-item">
-            <div className="item-images">
-              <img
-                src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
-                alt=""
-              />
-            </div>
-            <div className="item-actions">
-              <div className="item-info header">
-                <h3>Audi BMQ 2011</h3>
-                <p>15,560$</p>
-              </div>
-              <div className="item-timer">
-                <p>2 days and 15 hours left on the bid</p>
-              </div>
-              <div className="item-info">
-                <button>Place a bid</button>
-              </div>
-            </div>
-          </div>
-          <div className="posted-item">
-            <div className="item-images">
-              <img
-                src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
-                alt=""
-              />
-            </div>
-            <div className="item-actions">
-              <div className="item-info header">
-                <h3>Audi BMQ 2011</h3>
-                <p>15,560$</p>
-              </div>
-              <div className="item-timer">
-                <p>2 days and 15 hours left on the bid</p>
-              </div>
-              <div className="item-info">
-                <button>Place a bid</button>
-              </div>
-            </div>
-          </div>
-          <div className="posted-item">
-            <div className="item-images">
-              <img
-                src="https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_960_720.jpg"
-                alt=""
-              />
-            </div>
-            <div className="item-actions">
-              <div className="item-info header">
-                <h3>Audi BMQ 2011</h3>
-                <p>15,560$</p>
-              </div>
-              <div className="item-timer">
-                <p>2 days and 15 hours left on the bid</p>
-              </div>
-              <div className="item-info">
-                <button>Place a bid</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="recommendations"></div>
+      <Item />
     </div>
   );
 };
