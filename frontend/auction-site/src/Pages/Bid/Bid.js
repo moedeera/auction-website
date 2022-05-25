c493d8bf0f870b5ca5595511f665541a073d4512
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-25 01:12:42 -0600

    bid page css

--- frontend/auction-site/src/Pages/Bid/Bid.js
+++ frontend/auction-site/src/Pages/Bid/Bid.js
@@ -18,33 +18,36 @@ export const Bid = () => {
     <div className="container">
       <div className="bid-container">
         <div className="bid">
-          <div className="bid-item-header">
-            <h2>{bid?.title}</h2>
-            <h3>Bid:{bid?.price}</h3>
-            <div className="bid-details">
-              <p>5-0 Liter V9, 4WD, 8.9/10 AutoFax </p>
+          <div className="bid-upper-segment">
+            <div className="bid-item-header">
+              <h2>{bid?.title}</h2>
+              <h3>Bid:{bid?.price}</h3>
+              <div className="bid-details">
+                <p>5-0 Liter V9, 4WD, 8.9/10 AutoFax </p>
+              </div>
             </div>
-          </div>
-          <div className="bid-watch">
-            <div className="item-timer bid-item-timer">
-              {/* <p>
+            <div className="bid-watch">
+              <div className="item-timer bid-item-timer">
+                {/* <p>
                   {item.days} days {item.hours} hours left on bid
                 </p> */}
-              <div className="time-unit">
-                <h3 style={{ margin: "0" }}>0{bid.days}</h3>
-                <p style={{ margin: "0" }}>Days</p>
+                <div className="time-unit">
+                  <h3 style={{ margin: "0" }}>0{bid.days}</h3>
+                  <p style={{ margin: "0" }}>Days</p>
+                </div>
+                <div className="time-unit">
+                  <h3 style={{ margin: "0" }}>{bid.hours}</h3>
+                  <p style={{ margin: "0" }}>Hours</p>
+                </div>
+                <div className="time-unit">
+                  <h3 style={{ margin: "0" }}>{bid.hours + 27}</h3>
+                  <p style={{ margin: "0" }}>Minutes</p>
+                </div>
               </div>
-              <div className="time-unit">
-                <h3 style={{ margin: "0" }}>{bid.hours}</h3>
-                <p style={{ margin: "0" }}>Hours</p>
-              </div>
-              <div className="time-unit">
-                <h3 style={{ margin: "0" }}>{bid.hours + 27}</h3>
-                <p style={{ margin: "0" }}>Minutes</p>
-              </div>
+              <button className="btn btn-alternate ">bid </button>
             </div>
-            <button className="btn btn-alternate ">bid </button>
           </div>
+
           <div className="bid-images">
             <img src={bid?.pic} alt="" />
             <div className="bid-images-grid">
