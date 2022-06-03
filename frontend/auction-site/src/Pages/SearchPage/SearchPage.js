60b75a78c31ff27bbaea4a477e1b448a374d4198
moedeera <82620264+moedeera@users.noreply.github.com>
2022-06-03 03:10:02 -0600

    search-page-css-partly -done

--- frontend/auction-site/src/Pages/SearchPage/SearchPage.js
+++ frontend/auction-site/src/Pages/SearchPage/SearchPage.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { SiteContext } from "../../Context/Context";
+import { Link } from "react-router-dom";
 import "./SearchPage.css";
 
 export const SearchPage = () => {
-  const { type } = useParams();
+  const { currentCars } = useContext(SiteContext);
 
-  return <div className="container">You searched for {type} </div>;
+  const { type } = useParams();
+  const num = 3;
+
+  return (
+    <div className="container">
+      <div className="search-page-container">
+        <div className="search-page-header">
+          <h2>
+            {type} ({num}) results
+          </h2>
+        </div>
+        <div className="search-filter">
+          <ul>
+            <li>Newest</li>
+            <li>Lowest Price</li>
+            <li>Highest Price</li>
+            <li>Ending Soon</li>
+          </ul>
+        </div>
+        <div className="search-results">
+          {currentCars.map((car) => (
+            <div className="search-result" key={car.id}>
+              <img src={car?.pic} alt="" />
+
+              <div className="search-info">
+                <div className="info-header">
+                  <h3> {car.title}</h3>
+                  <h3 style={{ color: "steelblue" }}>{car.price}</h3>
+                </div>
+
+                <h4> {car.km},000km</h4>
+                <p>
+                  {car.days} days and {car.hours} hours left
+                </p>
+                <p style={{ color: "grey" }}>New Jersey, NW</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
