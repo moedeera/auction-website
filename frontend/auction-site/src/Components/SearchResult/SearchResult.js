99439baced0e876ad253dd6e1b5d7738960149f5
moedeera <82620264+moedeera@users.noreply.github.com>
2022-06-03 05:12:51 -0600

    search-page-css -done

--- frontend/auction-site/src/Components/SearchResult/SearchResult.js
+++ frontend/auction-site/src/Components/SearchResult/SearchResult.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./SearchResult.css";
 import { Link } from "react-router-dom";
 
-export const SearchResult = ({ searchResults }) => {
+export const SearchResult = ({ searchResults, order }) => {
   return (
     <div className="search-results">
       {searchResults.map((car) => (
-        <Link to={`/bid/${car.id}`}>
-          <div className="search-result" key={car.id}>
+        <Link to={`/bid/${car.id}`} key={car.id}>
+          <div className="search-result">
             <img src={car?.pic} alt="" />
 
             <div className="search-info">
               <div className="info-header">
                 <h3> {car.title}</h3>
-                <h3 style={{ color: "steelblue" }}>{car.price}</h3>
+                <h3 style={{ color: "green" }}>{car.price}</h3>
               </div>
 
               <h4> {car.km},000km</h4>
--- frontend/auction-site/src/Pages/SearchPage/SearchPage.js
+++ frontend/auction-site/src/Pages/SearchPage/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SiteContext } from "../../Context/Context";
 import { SearchResult } from "../../Components/SearchResult/SearchResult";
@@ -7,9 +7,62 @@ import "./SearchPage.css";
 
 export const SearchPage = () => {
   const { currentCars } = useContext(SiteContext);
-
+  let cars = currentCars;
   const { type } = useParams();
   const num = currentCars.length;
+  const [filter, setFilter] = useState(false);
+  const [underlined, setUnderlined] = useState(null);
+
+  const filterHandler = (type) => {
+    if (type === "mileage") {
+      if (filter) {
+        cars = cars.sort(function (a, b) {
+          return a.days - b.days;
+        });
+      } else {
+        cars = cars.sort(function (a, b) {
+          return b.days - a.days;
+        });
+      }
+    }
+    if (type === "price") {
+      if (filter) {
+        cars = cars.sort(function (a, b) {
+          return a.cost - b.cost;
+        });
+      } else {
+        cars = cars.sort(function (a, b) {
+          return b.cost - a.cost;
+        });
+      }
+    }
+
+    if (type === "year") {
+      if (filter) {
+        cars = cars.sort(function (a, b) {
+          return a.year - b.year;
+        });
+      } else {
+        cars = cars.sort(function (a, b) {
+          return b.year - a.year;
+        });
+      }
+    }
+
+    if (type === "time") {
+      if (filter) {
+        cars = cars.sort(function (a, b) {
+          return a.days + a.hours - (b.days + b.hours);
+        });
+      } else {
+        cars = cars.sort(function (a, b) {
+          return b.days + b.hours - (a.days + a.hours);
+        });
+      }
+    }
+
+    setFilter(!filter);
+  };
 
   return (
     <div className="container" style={{ border: "none" }}>
@@ -20,13 +73,45 @@ export const SearchPage = () => {
           </h2>
         </div>
         <div className="search-filter">
-          <div>Newest</div>
-          <div>Lowest Price</div>
-          <div>Highest Price</div>
-          <div>Ending Soon</div>
+          <div
+            style={underlined === 0 ? { backgroundColor: "#abdbe380" } : {}}
+            onClick={() => {
+              filterHandler("year");
+              setUnderlined(0);
+            }}
+          >
+            Year
+          </div>
+          <div
+            style={underlined === 1 ? { backgroundColor: "#abdbe380" } : {}}
+            onClick={() => {
+              filterHandler("mileage");
+              setUnderlined(1);
+            }}
+          >
+            Mileage
+          </div>
+          <div
+            style={underlined === 2 ? { backgroundColor: "#abdbe380" } : {}}
+            onClick={() => {
+              filterHandler("price");
+              setUnderlined(2);
+            }}
+          >
+            Price
+          </div>
+          <div
+            style={underlined === 3 ? { backgroundColor: "#abdbe380" } : {}}
+            onClick={() => {
+              filterHandler("time");
+              setUnderlined(3);
+            }}
+          >
+            Auction time
+          </div>
         </div>
 
-        <SearchResult searchResults={currentCars} />
+        <SearchResult searchResults={cars} />
       </div>
     </div>
   );
